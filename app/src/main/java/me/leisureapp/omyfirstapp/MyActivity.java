ab88a319c365c275e47be8e7f7ad2ad4bb6ba2e0
AWS Server <668Jerry@gmail.com>
2015-04-21 01:43:33 +0800

    test intent

--- app/src/main/java/me/leisureapp/omyfirstapp/MyActivity.java
+++ app/src/main/java/me/leisureapp/omyfirstapp/MyActivity.java
@@ -1,19 +1,23 @@
 package me.leisureapp.omyfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "me.leisureapp.omyfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +39,13 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
