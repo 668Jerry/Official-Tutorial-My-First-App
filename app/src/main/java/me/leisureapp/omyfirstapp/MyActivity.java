d56c7a050364b00411b41db3284810ab1f6b1552
AWS Server <668Jerry@gmail.com>
2015-04-24 11:32:30 +0800

    before Add Up Button for Low-level Activities

--- app/src/main/java/me/leisureapp/omyfirstapp/MyActivity.java
+++ app/src/main/java/me/leisureapp/omyfirstapp/MyActivity.java
@@ -29,8 +29,6 @@ public class MyActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -38,7 +36,15 @@ public class MyActivity extends ActionBarActivity {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void sendMessage(View view) {
